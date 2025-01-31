9933bc01dd67af8653009301b206375d5f5fdd06
SofiaLaura2008 <160143058+SofiaLaura2008@users.noreply.github.com>
2025-01-31 09:09:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-import os
 import pandas as pd
 import streamlit as st
 import plotly.express as px
 from sklearn.ensemble import RandomForestRegressor
 
+#funcao para carregar o dataset
+#@st.cache
 def get_data():
-    if not os.path.exists('data.csv'):
-        raise FileNotFoundError("O arquivo 'data.csv' não foi encontrado!")
-    return pd.read_csv('data.csv', encoding='utf-8')
+    return pd.read_csv('data.csv')
 
 def train_model():
     data = get_data()
-    x = data[['CRIM', 'INDUS', 'CHAS', 'NOX', 'RM', 'PTRATIO']]  
+    x = data.drop('MEDV', axis=1)
     y = data['MEDV']
     rf_regressor = RandomForestRegressor()
     rf_regressor.fit(x, y)
@@ -30,7 +29,7 @@ cols = st.multiselect('Atributos', data.columns.tolist(), default=defaultcols)
 st.dataframe(data[cols].head(10))
 st.subheader('Distribuição de imóveis por preço')
 
-faixa_valores = st.slider('Faixa de preço', float(data.MEDV.min()), float(data.MEDV.max()), (10.0, 100.0))
+faixa_valores = st.slider('Faixa de preço', float(data.MEDV.min()), 150., (10.0, 100.0))
 dados = data[data['MEDV'].between(left=faixa_valores[0], right=faixa_valores[1])]
 
 f = px.histogram(dados, x='MEDV', nbins=100, title='Distribuição de Preços')
@@ -41,16 +40,16 @@ st.plotly_chart(f)
 st.sidebar.subheader('Defina os atributos dos imóveis para predição')
 
 crim = st.sidebar.number_input('Taxa de criminalidade', value=data.CRIM.mean())
-indus = st.sidebar.number_input("Proporção de hectares de Indústrias", value=data.INDUS.mean())
+indus = st.sidebar.number_input("Proporção de hectares de Industrias", value=data.INDUS.mean())
 chas = st.sidebar.selectbox('Faz limite com o rio?', ('Sim', 'Não'))
 chas = 1 if chas == "Sim" else 0
 nox = st.sidebar.number_input('Concentração de óxido nítrico', value=data.NOX.mean())
-rm = st.sidebar.number_input('Número de quartos', value=data.RM.mean())
+rm = st.sidebar.number_input('Número de quartos', value=1)
 pratio = st.sidebar.number_input('Índice de alunos por professores', value=data.PTRATIO.mean())
 
 btn_predict = st.sidebar.button('Realizar predição')
 if btn_predict:
     result = model.predict([[crim, indus, chas, nox, rm, pratio]])
     st.subheader('O valor previsto para o imóvel é: ')
-    result = 'US $ ' + str(round(result[0], 2))
+    result = 'US $ '+str(round(result[0]*10,2))
     st.write(result)
